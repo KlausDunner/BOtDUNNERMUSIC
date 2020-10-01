279461a2741ae693edc8bf8b5a1f2c14eda83107
ll-Pixula-ll <69878303+ll-Pixula-ll@users.noreply.github.com>
2020-10-01 17:27:38 -0300

    bugs fixed

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,7 @@
 module.exports = {
     name: 'ready',
     execute: async function (client) {
-      console.log(`${client.user.username} is ready !`);
+      console.log(`${client.user.username} esta listo !`);
       client.infoApp = await client.fetchApplication();
       setInterval(async () => {
         client.infoApp = await client.fetchApplication();
